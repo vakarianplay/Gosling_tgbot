e3e6a1cea176c5f61bbe7e5f9e738f62446eabf1
Kostya Vakarian <vakarianplay@gmail.com>
2024-12-19 12:47:57 +0300

    httpserver init

---
+++ Baza_tgbot/httpserver.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var dbTick *sql.DB
+
+// var contentTbl string
+
+func HTTPServer(root_path, port_ string, db_ *sql.DB) {
+	dbTick = db_
+
+	// ticker := time.NewTicker(5 * time.Second)
+
+	// for range ticker.C {
+	// 	log.Println("tick "+root_path, " - "+port)
+	// 	ans, au := viewBaseLine()
+	// 	// ans = contentCmd + "\"" + ans + "\""
+	// 	// au = authorCmd + "\"" + au + "\""
+	// 	log.Println(ans, "    ", au)
+	// }
+
+	http.HandleFunc(root_path, func(w http.ResponseWriter, r *http.Request) {
+		ans, author := viewBaseLine()
+		response := fmt.Sprintf("%s   Автор: %s", ans, author)
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(response))
+	})
+	log.Println("Server listening on ", port_)
+	http.ListenAndServe(":"+port_, nil)
+
+}
+
+func viewBaseLine() (string, string) {
+	// qRnd := strings.Replace(QgetRandom, "{table}", contentTable, -1)
+	qRnd := strings.Replace(QgetRandom, "{table}", readCfg()[2], -1)
+	result, err := db.Query(qRnd)
+	if err != nil {
+		panic(err)
+	}
+	defer result.Close()
+
+	var id int
+	var baseLine string
+	var author string
+	var tgid string
+	result.Next()
+	if err := result.Scan(&id, &baseLine, &author, &tgid); err != nil {
+		panic(err)
+	}
+	ans := cleanString(baseLine)
+	// log.Println(ans)
+	return ans, author
+}
+
+func cleanString(input string) string {
+	cleaned := strings.ReplaceAll(input, "\n", " ")
+	cleaned = strings.ReplaceAll(cleaned, "\"", "")
+	cleaned = strings.ReplaceAll(cleaned, "'", "")
+
+	return cleaned
+}
